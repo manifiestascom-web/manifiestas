04dec17bfca30c401d2a0a9a5bbf6c6b6c009f30
Struky App <strukyapp@Strukys-MacBook-Air.local>
2026-06-26 19:27:22 -0500

    chore: disable meta pixel autoconfig

--- coach-app/src/components/analytics/MetaPixel.tsx
+++ coach-app/src/components/analytics/MetaPixel.tsx
@@ -36,6 +36,7 @@ export default function MetaPixel() {
             t.src=v;s=b.getElementsByTagName(e)[0];
             s.parentNode.insertBefore(t,s)}(window, document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('set', 'autoConfig', false, '${fpixel.FB_PIXEL_ID}');
             fbq('init', '${fpixel.FB_PIXEL_ID}');
             fbq('track', 'PageView');
           `,
